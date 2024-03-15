965f5d82e609ea045439a55187ed5e36dcebbf35
Nasirahamad88 <onlineworkbd88888@gmail.com>
2024-03-15 10:26:57 +0600

    done

--- app/lib/sanity.jsx
+++ app/lib/sanity.jsx
@@ -2,11 +2,12 @@ import { createClient } from "next-sanity";
 import ImageUrlBuilder from '@sanity/image-url'
 
 export const client = createClient({
-    projectId: '95jz6gzw',
-    dataset: 'production',
-    apiVersion: '2024-02-02',
-    useCdn: true,
-})
+  projectId: '95jz6gzw',
+  // projectId: "y3pbh6ki",
+  dataset: "production",
+  apiVersion: "2024-02-02",
+  useCdn: true,
+});
 
 const imgBuilder = ImageUrlBuilder(client);
 
